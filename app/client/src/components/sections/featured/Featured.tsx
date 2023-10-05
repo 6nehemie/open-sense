e082020ce355a6cba6c2fdaf27595d71b956b380
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-05 16:02:02 +0200

    card swap

--- app/client/src/components/sections/featured/Featured.tsx
+++ app/client/src/components/sections/featured/Featured.tsx
@@ -1,19 +1,9 @@
-import { coursesFeatured } from '../../../constants';
 import CourseCards from './CourseCards';
 
 const Featured = () => {
   return (
     <section className="relative w-full">
-      <div className="p-side">
-        <div className="w-full max-w-screen-wide mx-auto">
-          <h2 className="heading-2 text-right">
-            {coursesFeatured.titleLanding}
-          </h2>
-        </div>
-      </div>
-      <div className="max-[1634px]:">
-        <CourseCards />
-      </div>
+      <CourseCards />
     </section>
   );
 };
